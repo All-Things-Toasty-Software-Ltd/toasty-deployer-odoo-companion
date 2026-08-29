9b5d85a645ffe9a9239dbb15a2a8d70bac129505
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-29 19:59:04 +0100

    Add(Owner Model): Added the model to represent a repository owner. The url, avatar, and bio are all computed and taken from GitHub's API so long as there is a name (so the api can be called for it.)

---
+++ toasty_deployer/models/toasty_deployer_owner.py
@@ -0,0 +1,41 @@
+from odoo impprt models, fields, api
+import requests
+
+class ToastyDeployerOwner(models.Model):
+    _name = 'toasty.deployer.owner'
+    _description = 'Toasty Deployer Owner'
+
+    name = fields.Char(string='Owner Name', required=True, index=True)
+    github_url = fields.Char(string='GitHub Page', compute='_compute_github_url')
+    avatar_url = fields.Char(string='Avatar Page', compute='_compute_avatar_url')
+    bio = fields.Char(string='Bio / Description', compute='_compute_bio')
+    repo_ids = fields.One2many('toasty_deployer/repo', 'owner_id', string="Repositories")
+
+    @api.depends('name')
+    def _compute_github_url(self):
+        for rec in self:
+            rec.github_url = f"https://github.com/{rec.name}" if rec.name else "#"
+
+    @api.depends('name')
+    def _compute_avatar_url(self):
+        for rec in self:
+            if not rec.name:
+                continue
+            res = requests.get(f"https://api.github.com/users/{rec.name}", timeout=10)
+            if res.status_code == 200:
+                data = res.json()
+                rec.write({
+                    'avatar_url': data.get('avatar_url'),
+                })
+
+    @api.depends('name')
+    def _compute_avatar_url(self):
+        for rec in self:
+            if not rec.name:
+                continue
+            res = requests.get(f"https://api.github.com/users/{rec.name}", timeout=10)
+            if res.status_code == 200:
+                data = res.json()
+                rec.write({
+                    'bio': data.get('bio'),
+                })
