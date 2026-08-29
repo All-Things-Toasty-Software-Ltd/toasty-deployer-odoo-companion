e739e93ba47ce85272f7427347b851f78497f8c3
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-29 19:40:10 +0100

    Add(Settings): Add settings handling to easily customise the URL where Toasty Deployer is located.

--- toasty_deployer/models/__init__.py
+++ toasty_deployer/models/__init__.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Toasty Deployer. See LICENSE file for full copyright and licensing details.
+
+# inherit
+from . import res_config_settings
---
+++ toasty_deployer/models/res_config_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Toasty Deployer. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    toasty_deployer_base_url = fields.Char(string="Toasty Deployer Server URL",
+                                           help="The Base URL where your Toasty Deployer instance is hosted.",
+                                           readonly=False, required=True, config_parameter='toasty_deployer.base_url',
+                                           default="https://deployer.toastysoftware.co.uk")
